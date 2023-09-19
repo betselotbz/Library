c30d78b2045e0e6629e833563f47f2457779e34a
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-19 11:22:17 -0500

    added getters and setters, also default constructor

--- Book/src/main/java/com/example/book/model/User.java
+++ Book/src/main/java/com/example/book/model/User.java
@@ -20,4 +20,47 @@ public class User {
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
+    public User() {
+        //default constructor
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
