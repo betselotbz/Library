71a56c76f3c81de77a3aada797d317dd06e7b2a9
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-19 10:35:22 -0500

    added default constructor and parameterized constructor

--- Book/src/main/java/com/example/book/model/Book.java
+++ Book/src/main/java/com/example/book/model/Book.java
@@ -1,15 +1,32 @@
 package com.example.book.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "books")
 public class Book {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column
     private String title;
-    private String Author;
+    @Column
+    private String author;
+    @Column
     private Long pages;
+    @Column
     private boolean available;
 
+    public Book() {
+        //default constructor
+    }
+
+    public Book(Long id, String title, String author, Long pages, boolean available) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.available = available;
+    }
 }
