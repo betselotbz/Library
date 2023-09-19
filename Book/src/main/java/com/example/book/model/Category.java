cc147ad83fd21bea5855aea4807e5d4e2a5178c1
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-19 07:24:59 -0500

    added instance variables to Category class

--- Book/src/main/java/com/example/book/model/Category.java
+++ Book/src/main/java/com/example/book/model/Category.java
@@ -6,7 +6,14 @@ import javax.persistence.*;
 @Table(name = "categories")
 public class Category {
 
-
+    @Id //marks the id field as the primary key of our entity class.
+    @Column // specifies that a field is mapped to a database column.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //automatically generate unique ID values
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private String description;
 
 
 }
