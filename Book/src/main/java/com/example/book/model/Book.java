0c600508fc433879483959610e84ba7abf2fe687
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-19 10:42:47 -0500

    added getters and setters

--- Book/src/main/java/com/example/book/model/Book.java
+++ Book/src/main/java/com/example/book/model/Book.java
@@ -29,4 +29,44 @@ public class Book {
         this.pages = pages;
         this.available = available;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Long getPages() {
+        return pages;
+    }
+
+    public void setPages(Long pages) {
+        this.pages = pages;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
 }
