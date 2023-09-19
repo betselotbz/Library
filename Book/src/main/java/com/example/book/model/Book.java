af95ad3da1b694856c8df4f56243c0fe1e42e1c0
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-19 06:27:51 -0500

    created Category class

---
+++ Book/src/main/java/com/example/book/model/Book.java
@@ -0,0 +1,9 @@
+package com.example.book.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categories")
+public class Book {
+}
---
+++ Book/src/main/java/com/example/book/model/Category.java
@@ -0,0 +1,12 @@
+package com.example.book.model;
+
+import javax.persistence.*;
+
+
+public class Category {
+
+
+
+
+}
+
