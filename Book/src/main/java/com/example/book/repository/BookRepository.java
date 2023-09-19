d7b0ce211adb85fb8b8cc34632a612bbfffcdd9d
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-19 18:35:39 -0500

    methods for querying and retrieving books

--- Book/src/main/java/com/example/book/repository/BookRepository.java
+++ Book/src/main/java/com/example/book/repository/BookRepository.java
@@ -1,9 +1,15 @@
 package com.example.book.repository;
 
 import com.example.book.model.Book;
+import com.example.book.model.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookRepository extends JpaRepository <Book, Long> {
+    Book findByName(String bookName);
+    Book findByIdAndUserId(Long categoryId, Long userId);
+    List<Book> findByUserId(Long userId);
 }
