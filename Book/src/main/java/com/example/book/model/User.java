5b36afd232ba0a9b93fceb0ddd8cff15aee956f6
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-19 11:20:39 -0500

    added attributes to User

--- Book/src/main/java/com/example/book/model/User.java
+++ Book/src/main/java/com/example/book/model/User.java
@@ -1,9 +1,23 @@
 package com.example.book.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
 public class User {
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column
+    private String name;
+    @Column
+    private String userName;
+    @Column(unique = true)
+    private String emailAddress;
+    @Column
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
 
 }
