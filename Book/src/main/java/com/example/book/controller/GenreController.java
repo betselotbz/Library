572b2660cc9cf6af24bad66b7d80f8781a9806c0
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-19 16:27:18 -0500

    created GenreServic class

--- Book/src/main/java/com/example/book/controller/GenreController.java
+++ Book/src/main/java/com/example/book/controller/GenreController.java
@@ -1,10 +1,11 @@
 package com.example.book.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api")
+@RestController//annotation indicates that this class will handle incoming HTTP requests and provide responses.
+@RequestMapping("/api")//http://localhost:9094/api
+// annotation specifies the base path for the controller's endpoints, meaning that all endpoints defined within this controller will start with "/api" in their URL paths
 public class GenreController {
-
 }
