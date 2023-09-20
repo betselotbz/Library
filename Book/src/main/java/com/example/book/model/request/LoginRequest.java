00d420da3b3eeeebd1ab0403bd689aa0b4ea5477
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-20 01:53:48 -0500

    Add email and password fields to LoginRequest class

--- Book/src/main/java/com/example/book/model/request/LoginRequest.java
+++ Book/src/main/java/com/example/book/model/request/LoginRequest.java
@@ -1,4 +1,16 @@
 package com.example.book.model.request;
 
 public class LoginRequest {
+    private String emailAddress;
+    private String password;
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
+/*It contains two private fields, emailAddress and password, along with corresponding getter methods to access these fields.
+This class is typically used to encapsulate user-provided login information, such as email address and password, when making login requests to a server or application.*/
