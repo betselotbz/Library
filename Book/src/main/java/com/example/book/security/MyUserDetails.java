e085320d5336a7cf52266c7302bb6d646d8d6c6e
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-20 16:22:02 -0500

    implement methods in UserDetailsService

--- Book/src/main/java/com/example/book/security/MyUserDetails.java
+++ Book/src/main/java/com/example/book/security/MyUserDetails.java
@@ -11,32 +11,32 @@ public class MyUserDetails implements UserDetails {
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
     }
-
+    // Returns the user's password.
     @Override
     public String getPassword() {
         return null;
     }
-
+    // Returns the user's username
     @Override
     public String getUsername() {
         return null;
     }
-
+    //Shows if user's account has expired
     @Override
     public boolean isAccountNonExpired() {
         return false;
     }
-
+    //Shows if user's account is locked
     @Override
     public boolean isAccountNonLocked() {
         return false;
     }
-
+    //Shows if user's credential's have expired (like password)
     @Override
     public boolean isCredentialsNonExpired() {
         return false;
     }
-
+    //Shows if user account is active
     @Override
     public boolean isEnabled() {
         return false;
