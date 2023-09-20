79e09992b3a44ef6beefaab60981063466fbffe2
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-20 02:21:01 -0500

    added UserController class

---
+++ Book/src/main/java/com/example/book/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.example.food.controller;
+
+import com.example.book.model.User;
+import com.example.book.model.request.LoginRequest;
+
+import com.example.book.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/auth/users") // http://localhost:9092/auth/users/
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(path = "/register/") // http://localhost:9092/auth/users/register/
+    public User createUser(@RequestBody User userObject) {
+        return userService.createUser(userObject);
+    }
+}
