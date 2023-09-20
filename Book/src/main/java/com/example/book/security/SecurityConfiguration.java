d5b882612f51143f071113e84cf074d11c180af5
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-20 17:18:58 -0500

    Created a Password encoder bean

--- Book/src/main/java/com/example/book/security/SecurityConfiguration.java
+++ Book/src/main/java/com/example/book/security/SecurityConfiguration.java
@@ -1,4 +1,14 @@
 package com.example.book.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class SecurityConfiguration {
+// Define a BCryptPasswordEncoder bean for password encoding.
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
 }
