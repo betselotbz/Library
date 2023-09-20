f73ae61fc759ee123314bb66298ba8a82671253f
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-20 02:04:28 -0500

    added constructor for the UserService

--- Book/src/main/java/com/example/book/service/UserService.java
+++ Book/src/main/java/com/example/book/service/UserService.java
@@ -1,7 +1,17 @@
 package com.example.book.service;
 
+import com.example.book.repository.UserRepository;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
+    public UserService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
-
+    // Rest of the UserService class methods...
 }
