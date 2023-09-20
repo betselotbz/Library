4311acf33289380e02204f06975fc4f5b68db46c
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-20 00:47:09 -0500

    Add GET endpoint for retrieving genre id from genre

--- Book/src/main/java/com/example/book/controller/GenreController.java
+++ Book/src/main/java/com/example/book/controller/GenreController.java
@@ -3,12 +3,14 @@ package com.example.book.controller;
 import com.example.book.service.GenreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.book.service.BookService;
 import com.example.book.model.Genre;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController//annotation indicates that this class will handle incoming HTTP requests and provide responses.
 @RequestMapping("/api")//http://localhost:9094/api
@@ -19,8 +21,14 @@ public class GenreController {
     public GenreController(GenreService genreService) {
         this.genreService = genreService;
     }
-    @GetMapping(path = "/genres/") // http://localhost:9092/api/categories/
+    @GetMapping(path = "/genres/") // http://localhost:9094/api/genres/
     public List<Genre> getGenres() {
         return genreService.getGenres();
     }
+    @GetMapping(path = "/genres/{genreId}") // http://localhost:9092/api/genres/1/
+    public Optional<Genre> getGenre(@PathVariable(value = "genreId") Long genreId) {
+        return genreService.getGenre(genreId);
+    }
+
+
 }
