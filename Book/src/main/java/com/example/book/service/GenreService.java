b295719782567c985d6d6ffbaa13bf246b11075a
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-19 20:53:24 -0500

    create Genre

--- Book/src/main/java/com/example/book/service/GenreService.java
+++ Book/src/main/java/com/example/book/service/GenreService.java
@@ -1,5 +1,7 @@
 package com.example.book.service;
 
+import com.example.book.exception.InformationExistException;
+import com.example.book.model.Genre;
 import com.example.book.repository.BookRepository;
 import com.example.book.repository.GenreRepository;
 import org.springframework.stereotype.Service;
@@ -15,11 +17,26 @@ public class GenreService {
     public void setGenreRepository(GenreRepository genreRepository) {
         this.genreRepository = genreRepository;
     }
-    //assigning instances of 'GenreRepository'
+
     public void setBookRepository(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
-    //assigning instances of 'BookRepository'
 
-    // These setter methods are used to inject (set) the GenreRepository and BookRepository instances into this service.
+    /* assigning instances of 'GenreRepository'
+     assigning instances of 'GenreRepository'
+    These setter methods are used to inject (set) the GenreRepository and BookRepository instances into this service.
+    @Autowired helps Spring provide instances of CategoryRepository and RecipeRepository and sets them as dependencies for your class.*/
+
+    public Genre createGenre(Genre genreObject) {
+        Genre genre = genreRepository.findByName(genreObject.getName());
+        if (genre != null) {
+            throw new InformationExistException("genre with name " + genreObject.getName() + " already exists");
+        } else {
+            return genreRepository.save(genreObject);
+        }
+    }
+    /*creating a new genre in an application.
+     It checks if a genre with the same name already exists in the database using genreRepository.
+     If it does, it throws an exception.
+     If not, it creates and saves the new genre to the database */
 }
