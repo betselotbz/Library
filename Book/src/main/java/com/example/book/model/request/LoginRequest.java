260c170ac56c76430e0381f38867a12a6614aa11
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-20 01:41:00 -0500

    added LoginRequest class

---
+++ Book/src/main/java/com/example/book/model/request/LoginRequest.java
@@ -0,0 +1,4 @@
+package com.example.book.model.request;
+
+public class LoginRequest {
+}
--- Book/src/main/java/com/example/book/service/UserService.java
+++ Book/src/main/java/com/example/book/service/UserService.java
@@ -1,14 +1,7 @@
 package com.example.book.service;
 
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-
 public class UserService {
 
-    @Service
-    public class UserService {
 
-    ;
 
 }
