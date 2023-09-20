392af98418f9ef475f520a8911455a19983f90f2
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-20 00:16:15 -0500

    Add GenreService constructor injection

--- Book/src/main/java/com/example/book/controller/GenreController.java
+++ Book/src/main/java/com/example/book/controller/GenreController.java
@@ -1,5 +1,7 @@
 package com.example.book.controller;
 
+import com.example.book.service.GenreService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")//http://localhost:9094/api
 // annotation specifies the base path for the controller's endpoints, meaning that all endpoints defined within this controller will start with "/api" in their URL paths
 public class GenreController {
+    private GenreService genreService;
+    @Autowired
+    public GenreController(GenreService genreService) {
+        this.genreService = genreService;
+    }
+
 }
