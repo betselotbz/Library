a3ea07bdec0e83eeb92b9d893205d81cc37cc0b2
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-20 17:23:17 -0500

    configure the security filter chain

--- Book/src/main/java/com/example/book/security/SecurityConfiguration.java
+++ Book/src/main/java/com/example/book/security/SecurityConfiguration.java
@@ -1,14 +1,43 @@
 package com.example.book.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration // who can come into building
+@EnableGlobalMethodSecurity(prePostEnabled = true) //to get in the restricted rooms
 public class SecurityConfiguration {
-// Define a BCryptPasswordEncoder bean for password encoding.
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+    @Bean
+    public JwtRequestFilter authJwtRequestFilter() {
+        return new JwtRequestFilter();
+    }
 
+    @Bean //to use the below functionality we need to convert it to bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { //SecurityFilterChain comes from import not created by us
+        http.authorizeRequests().antMatchers("/auth/users", "/auth/users/login/", "/auth/users/register/").permitAll()
+                .antMatchers("/h2-console/**").permitAll() //**never enter this line of code
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().csrf().disable()
+                .headers().frameOptions().disable(); //**never enter this line of code
+        http.addFilterBefore(authJwtRequestFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
 
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
