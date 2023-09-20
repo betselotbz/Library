667b10890f3b4523a1188952d41c7f7c9fced14e
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-20 02:06:29 -0500

    added a method to create a new User

--- Book/src/main/java/com/example/book/service/UserService.java
+++ Book/src/main/java/com/example/book/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.book.service;
 
+import com.example.book.exception.InformationExistException;
+import com.example.book.model.User;
 import com.example.book.repository.UserRepository;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,5 +15,16 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    // Rest of the UserService class methods...
+    public User createUser(User userObject) {
+        // Check if a user with the same email address already exists
+        if (!userRepository.existsByEmailAddress(userObject.getEmailAddress())) {
+            // Encode the user's password for security
+            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
+            // Save the user to the database
+            return userRepository.save(userObject);
+        } else {
+            // Throw an exception if the email address is already in use
+            throw new InformationExistException("user with email address " + userObject.getEmailAddress() + " already exists");
+        }
+    }
 }
