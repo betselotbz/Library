5055e1dc14b106d62a2afe64133153d709e04de6
Betselot Bezabeh <betselotbz@gmail.com>
2023-09-21 16:51:31 -0500

    added documentations

--- Book/src/main/java/com/example/book/exception/InformationNotFoundException.java
+++ Book/src/main/java/com/example/book/exception/InformationNotFoundException.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class InformationNotFoundException extends RuntimeException {
 
+
     /*
      * Constructor for InformationNotFoundException.
      *
